597350ad7c6aea1170045401817a59da992882c3
Tony Hirst <tony.hirst@gmail.com>
2017-09-09 00:40:31 +0100

    vm config

--- robotVM/build/base/basedesktop.sh
+++ robotVM/build/base/basedesktop.sh
@@ -1,5 +1,20 @@
 #!/usr/bin/env bash
+THISDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
 
 #Install base python packages
 #apt-get update && apt-get install -y ubuntu-desktop && apt-get clean
 apt-get update && apt-get install -y --no-install-recommends ubuntu-desktop && apt-get install -y gnome-terminal unity-lens-applications unity-lens-files && apt-get clean
+
+
+#Autologin
+#https://askubuntu.com/a/51087
+cp $THISDIR/etc/lightdm/lightdm.conf /etc/lightdm/lightdm.conf
+
+
+
+#Desktop config settings to run at every boot
+cp $THISDIR/scripts/set-gnome-settings.sh /opt/set-gnome-settings.sh 
+chmod +x /opt/set-gnome-settings.sh 
+cp -r $THISDIR/home/vagrant/.config/autostart/. /home/vagrant/.config/autostart/
+chmod +x /home/vagrant/.config/autostart/*.desktop
---
+++ robotVM/build/base/scripts/set-gnome-settings.sh
@@ -0,0 +1,5 @@
+#dock location
+gsettings set com.canonical.Unity.Launcher launcher-position Bottom
+
+#screensaver disable
+gsettings set org.gnome.desktop.screensaver lock-enabled false
