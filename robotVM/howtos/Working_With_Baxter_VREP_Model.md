c36d6c1efd5ad134d6d5c963dfa657409aaf3a29
Tony Hirst <tony.hirst@gmail.com>
2017-11-14 20:17:10 +0000

    add baxter demo - first pass

---
+++ robotVM/howtos/Working_With_Baxter_VREP_Model.md
@@ -0,0 +1,61 @@
+# Working With Baxter VREP Model
+
+Notes on working with the Baxter VREP Model.
+
+### Script Set-Up In the Model / Scene
+
+To contrtol the Baxter model from a Python script via the reote API:
+
+- disable the *Threaded child script*s;
+- leave the [*Non-threaded child script (Baxter)*](../luascripts/Non_threaded_child_script_Baxter.lua) enabled (this manages the sensors)
+
+### Enabling Sensors
+
+The sensors are managed via the [*Non-threaded child script (Baxter)*](../luascripts/Non_threaded_child_script_Baxter.lua). Ensure the sensors are enabled as required:
+
+```lua
+local headSensorOn=true
+local headCameraOn=true
+local rightHandSensorsOn=true
+local leftHandSensorsOn=true
+```
+
+TO CHECK:
+
+To set the variables from a script it looks as if they need setting via a signal? http://www.forum.coppeliarobotics.com/viewtopic.php?f=9&t=4227 http://www.forum.coppeliarobotics.com/viewtopic.php?f=9&t=2695 
+ Add this in a threaded script somewhere? But then, how does it relate to var set as local in Unthreaded child script?
+
+### Adding Manipulators
+
+There are two manipulators defined for the Baxter available in the VREP simulator. They can befound in the *Model browser > components > grippers* panel:
+
+- gripper;
+- vacuum cup.
+
+To add the manipulators to the Baxter model:
+
+- drag the manipulator model into the scene;
+- in the *Scene hierarchy*, drag the manipulator object into the correct place in the hierarchy - for example, to the end of one of Baxter's arms:
+
+![](../images/V-REP_PRO_EDU_-_Baxter_add_manipulator.png)
+
+- connect the manipulator to the model in the rendered scene:
+  - select the manipulator;
+  - select the *Object/item shift* option from the toolbar;
+  - set the position relative to *Parent frame*.
+
+  
+#### Gripper
+
+![](../images/V-REP_PRO_EDU_-_Baxter_gripper.png)
+
+The gripper relative position setting (*Object/item shift* ) and orientation setting (from *Object/item rotate*) appear to be 0.
+
+#### Vacuum cup
+
+![](../images/V-REP_PRO_EDU_-_Baxter_vacuum_cup.png)
+
+Not sure if I'm doing something wrong here, but the gripper relative position setting (*Object/item shift* ) and orientation setting (from *Object/item rotate*) appear to be not-0:
+
+- relative position: -0.055 in z, +0.002 in y, 0.0015 in x
+- relative rotation: -180 degrees in Alpha
---
+++ robotVM/luascripts/Non_threaded_child_script_Baxter.lua
@@ -0,0 +1,77 @@
+------------------------------------------------------------------------------ 
+-- Following few lines automatically added by V-REP to guarantee compatibility 
+-- with V-REP 3.1.3 and earlier: 
+colorCorrectionFunction=function(_aShapeHandle_) 
+    local version=simGetInt32Parameter(sim_intparam_program_version) 
+    local revision=simGetInt32Parameter(sim_intparam_program_revision) 
+    if (version<30104)and(revision<3) then 
+        return _aShapeHandle_ 
+    end 
+    return '@backCompatibility1:'.._aShapeHandle_ 
+end 
+------------------------------------------------------------------------------ 
+ 
+ 
+-- This is a simple non-threaded example script that enables/disables some of Baxter's devices:
+
+if (sim_call_type==sim_childscriptcall_initialization) then 
+    local headSensorOn=true
+    local headCameraOn=true
+    local rightHandSensorsOn=true
+    local leftHandSensorsOn=true
+    local headLightColor={1,0,0}
+
+    for i=1,12,1 do
+        local h=simGetObjectHandle('Baxter_ultrasonic_sensor'..i)
+        if headSensorOn then
+            simSetExplicitHandling(h,0)
+        else
+            simSetExplicitHandling(h,1)
+            simResetProximitySensor(h)
+        end
+    end
+
+    local headCam=simGetObjectHandle('Baxter_camera')
+    if headCameraOn then
+        simSetExplicitHandling(headCam,0)
+    else
+        simSetExplicitHandling(headCam,1)
+        simResetVisionSensor(headCam)
+    end
+
+    local rightHandProx=simGetObjectHandle('Baxter_rightArm_proxSensor')
+    local rightHandCam=simGetObjectHandle('Baxter_rightArm_camera')
+    if rightHandSensorsOn then
+        simSetExplicitHandling(rightHandProx,0)
+        simSetExplicitHandling(rightHandCam,0)
+        local camView=simFloatingViewAdd(0.9,0.9,0.2,0.2,0)
+        simAdjustView(camView,rightHandCam,64)
+    else
+        simSetExplicitHandling(rightHandProx,1)
+        simResetProximitySensor(rightHandProx)
+        simSetExplicitHandling(rightHandCam,1)
+        simResetVisionSensor(rightHandCam)
+    end
+    
+    local leftHandProx=simGetObjectHandle('Baxter_leftArm_proxSensor')
+    local leftHandCam=simGetObjectHandle('Baxter_leftArm_camera')
+    if leftHandSensorsOn then
+        simSetExplicitHandling(leftHandProx,0)
+        simSetExplicitHandling(leftHandCam,0)
+        local camView=simFloatingViewAdd(0.1,0.9,0.2,0.2,0)
+        simAdjustView(camView,leftHandCam,64)
+    else
+        simSetExplicitHandling(leftHandProx,1)
+        simResetProximitySensor(leftHandProx)
+        simSetExplicitHandling(leftHandCam,1)
+        simResetVisionSensor(leftHandCam)
+    end
+
+    headLight=simGetObjectHandle('Baxter_headRing')
+    simSetShapeColor(colorCorrectionFunction(headLight),'',3,headLightColor)
+end 
+
+if (sim_call_type==sim_childscriptcall_cleanup) then 
+    simSetShapeColor(colorCorrectionFunction(headLight),'',3,{0,1,0})
+end 
+
